c140a6e7f7593596fc0102eaf2cbdf8e5d2fb398
zhangbw1 <572152808@qq.com>
2017-03-18 19:24:50 +0800

    第二次上传

--- src/main/java/Git/Gittest.java
+++ src/main/java/Git/Gittest.java
@@ -3,7 +3,9 @@ package Git;
 public class Gittest {
   
 	public static void main(String[] args) {
-		
+		int a = 1;
+		int b = 2;
+		System.out.println(a+b);
 		System.out.println("gitTest01");
 	}
 }
